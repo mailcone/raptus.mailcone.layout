7d86356de7160e20b9bab62cc986ac5d3424993e
samuel.riolo@googlemail.com <samuel.riolo@googlemail.com@2114add4-67ca-9ca6-10be-5e31bf341269>
2011-09-06 20:33:45 +0000

    create special navigation directive for automatically locate and display the menu item

--- mailcone/project/raptus.mailcone.layout/trunk/raptus/mailcone/layout/__init__.py
+++ mailcone/project/raptus.mailcone.layout/trunk/raptus/mailcone/layout/__init__.py
@@ -1,2 +1,9 @@
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('raptus.mailcone.layout')
+
+from raptus.mailcone.layout.navigation import locatormenuitem, LocatorMenuItem
+
+# Provide these directives here as well:
+from grokcore.component import name, title, description
+from grokcore.viewlet import order
+
---
+++ mailcone/project/raptus.mailcone.layout/trunk/raptus/mailcone/layout/meta.py
@@ -0,0 +1,63 @@
+import martian
+from martian.error import GrokError
+
+import grokcore.view
+import grokcore.viewlet
+import grokcore.security
+import grokcore.component
+
+from grokcore.view.meta.views import ViewSecurityGrokker, default_view_name
+from zope.browsermenu.metaconfigure import menuDirective, menuItemDirective, subMenuItemDirective
+from zope.configuration.exceptions import ConfigurationError
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.publisher.interfaces.browser import IBrowserView
+
+from megrok.navigation import directives
+from megrok.navigation.util import registerMenuItem
+from megrok.navigation.meta import MenuViewGrokker
+
+from raptus.mailcone.layout.navigation import locatormenuitem
+from raptus.mailcone.layout.navigation import LocatorMenuItem
+
+
+
+class MenuItemGrokker(ViewSecurityGrokker):
+    martian.directive(locatormenuitem)
+    martian.directive(grokcore.component.context)
+    martian.directive(grokcore.viewlet.order)
+    martian.directive(grokcore.view.layer, default=IDefaultBrowserLayer)
+    martian.directive(grokcore.component.name, get_default=default_view_name)
+    martian.directive(grokcore.component.title, default=u'')
+    martian.directive(grokcore.component.description, default=u'')
+    martian.directive(
+        grokcore.view.require, default="zope.View", name='permission')
+
+    def execute(self, factory, config, permission, order, context=None,
+                layer=None, name=u'', locatormenuitem=None, description=u'',
+                title=u''):
+        if locatormenuitem is None:
+            return False
+
+        locatormenu, (locator, title, order, icon, group) = locatormenuitem
+        title = title or viewtitle or name 
+        if martian.util.check_subclass(permission, grokcore.security.Permission):
+            permission =  grokcore.component.name.bind().get(permission)
+        item_name = name
+        item_itf = directives.itemsimplement.bind().get(locatormenu)
+        config.action(discriminator=('viewlet', None, layer,
+                         IBrowserView, locatormenu, item_name),
+                         callable=registerMenuItem,
+                         args=(factory.module_info, LocatorMenuItem, (None, layer, IBrowserView, locatormenu)
+                               , item_name, permission, item_itf, layer, 
+                               {'title':title,
+                                'viewName':name,
+                                'description':description,
+                                '_icon':icon,
+                                'group': group,
+                                'locator': locator
+                                },
+                                (order, MenuViewGrokker._dynamic_items),
+                                )
+                         )
+        MenuViewGrokker._dynamic_items+=1
+        return True
